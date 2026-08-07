a07a885079df6e889906df42754a23ea1afe45b5
dl369 <166696301+dl369@users.noreply.github.com>
2026-08-07 16:36:36 +1000

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        comp = {}
+
+        for i in range(len(nums)):
+            
+            diff = target - nums[i]
+
+            if diff in comp:
+                return sorted([i, comp[diff]])
+            
+            comp[nums[i]] = i 
