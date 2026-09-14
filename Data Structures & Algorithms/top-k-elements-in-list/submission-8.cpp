57550871f280eb240f1fca054f4ae038d3d28605
dl369 <166696301+dl369@users.noreply.github.com>
2026-09-14 19:00:14 +1000

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.cpp
@@ -0,0 +1,31 @@
+#include <ranges>
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> counts;
+        std::vector<vector<int>> freqs(nums.size() + 1);
+        std::vector<int> res;
+
+        for (const auto& num : nums) {
+            counts[num]++;
+        }
+
+        for (const auto& [val, count] : counts) {
+            freqs[count].push_back(val);
+        }
+
+        res.reserve(k);
+
+        for (const auto& v : std::views::reverse(freqs)) {    
+            for (const auto& num : v) {
+                res.push_back(num);
+                if (res.size() == k) {
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+};
